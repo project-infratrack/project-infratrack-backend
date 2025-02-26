fe0f38958f3e75cc35ece4956ceeb111ae816d2a
Chamo0Chiran <chamochiran05@gmail.com>
2025-02-26 13:20:45 +0530

    initial commit of springboot commit

---
+++ project-infratrack-backend/src/test/java/com/CS153/project_infratrack_backend/ProjectInfratrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS153.project_infratrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInfratrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
