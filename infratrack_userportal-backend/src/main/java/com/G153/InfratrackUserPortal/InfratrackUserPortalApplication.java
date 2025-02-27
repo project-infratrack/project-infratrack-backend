6181e3aff3f98d31832379406b31964034ae01b5
Chamo0Chiran <chamochiran05@gmail.com>
2025-02-27 21:38:57 +0530

    initial commit

---
+++ infratrack_userportal-backend/src/main/java/com/G153/InfratrackUserPortal/InfratrackUserPortalApplication.java
@@ -0,0 +1,14 @@
+package com.G153.InfratrackUserPortal;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfratrackUserPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfratrackUserPortalApplication.class, args);
+	}
+
+}
